3fcad27c19a32bbca9a14668437575e067df7118
Susmita4 <128575143+Susmita4@users.noreply.github.com>
2023-03-24 13:45:20 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -7,10 +7,10 @@ public class factorial {
 	}
 	
 	public static int factorial(int n)
-	{	int result = 1;
+	{	int result1 = 1;
 		for(int i = 2; i <= n; i++)
-			result *= i;
-		return result;
+			result1 *= i;
+		return result1;
 	}
 
 }
